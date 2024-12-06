dcc4502d42536cfbd7a79ecb49fe721bad19e387
mgalal <squareno2@gmail.com>
2024-12-06 18:48:41 +0000

    Add new blog post on mastering Laravel testing with Xdebug and update sitemap and RSS feeds

--- new-post.py
+++ new-post.py
@@ -1,7 +1,10 @@
 import os
 title = input("Title: ")
-# lower case remove spaces and replace with dashes
-title = title.lower().replace(" ", "-")
+# lower case remove spaces and replace with dashes, remove special characters
+title = "".join([c if c.isalnum() else "-" for c in title]).lower()
+title = title.replace(" ", "-")
+
+# title = title.lower().replace(" ", "-")
 # loop over the folders in the content directory and let the user select one
 folders = [f for f in os.listdir("content") if os.path.isdir(os.path.join("content", f))]
 for i, folder in enumerate(folders):
